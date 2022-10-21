2b330b5735708b170f9cac86e42310c44ec21898
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-21 17:24:47 +0100

    update

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -2,12 +2,16 @@ import React from 'react'
 import BottomNav from '../../components/BottomNav'
 import HomeNav from '../../components/HomeNav'
 import { foodNotActive, homeActive, orderNotActive, profileNotActive } from '../../utility/tools'
+import "./Homepage.css"
 
 function Homepage() {
   return (
     <>
     <HomeNav/>
-    
+    <div className="homepage__cnt">
+        <p className="greeting">Good Morning 9trocoder,</p>
+        <p className="happy">Happy Liasu Clan Day!</p>
+    </div>
         <BottomNav homeIcon={homeActive} foodIcon={foodNotActive} orderIcon={orderNotActive} profileIcon={profileNotActive} />
     </>
   )
