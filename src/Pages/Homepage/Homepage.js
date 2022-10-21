1697979ec3efedf29474f0a153f9f75ba044e9b7
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-21 17:14:42 +0100

    worked on the homepage

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BottomNav from '../../components/BottomNav'
+import HomeNav from '../../components/HomeNav'
 import { foodNotActive, homeActive, orderNotActive, profileNotActive } from '../../utility/tools'
 
 function Homepage() {
   return (
     <>
+    <HomeNav/>
     
         <BottomNav homeIcon={homeActive} foodIcon={foodNotActive} orderIcon={orderNotActive} profileIcon={profileNotActive} />
     </>
