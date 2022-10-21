3ec9132bed6f469a635e3b21b0cebcce202db252
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-21 21:09:19 +0100

    update

--- src/components/FoodTray.js
+++ src/components/FoodTray.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { delivery, stars, time } from "../utility/tools";
 
 function FoodTray({ suggestion, suggestionlabel }) {
   return (
@@ -10,10 +11,46 @@ function FoodTray({ suggestion, suggestionlabel }) {
 
       <div className="foodtray__cnt">
         <div className="foodtray__cnt-items">
-            <img src="https://images.unsplash.com/photo-1638436684761-7e59f8a9072f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
+          <img
+            src="https://images.unsplash.com/photo-1638436684761-7e59f8a9072f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+            alt=""
+          />
+          <p className="foodtray__title">Fried Rice</p>
+          <p className="foodtray__paragraph">
+            A delicious fried rice food from Nigeria.
+          </p>
+          <div className="foodtray__bottom">
+            <div className="foodtray__bottom-item">
+              {time} <p>10 min</p>
+            </div>
+            <div className="foodtray__bottom-item">
+              {stars} <p>4.5</p>
+            </div>
+            <div className="foodtray__bottom-item">
+              {delivery} <p>Free Delivery</p>
+            </div>
+          </div>
         </div>
         <div className="foodtray__cnt-items">
-            <img src="https://images.unsplash.com/photo-1629606600908-a51ec6c44fdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80" alt="" />
+          <img
+            src="https://images.unsplash.com/photo-1629606600908-a51ec6c44fdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
+            alt=""
+          />
+          <p className="foodtray__title">Fried Rice</p>
+          <p className="foodtray__paragraph">
+            A delicious fried rice food from Nigeria.
+          </p>
+          <div className="foodtray__bottom">
+            <div className="foodtray__bottom-item">
+              {time} <p>10 min</p>
+            </div>
+            <div className="foodtray__bottom-item">
+              {stars} <p>4.5</p>
+            </div>
+            <div className="foodtray__bottom-item">
+              {delivery} <p>Free Delivery</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
