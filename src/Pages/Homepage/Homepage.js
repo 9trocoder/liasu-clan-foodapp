7a51a8b354b2c7c578e38c87b587d6a3be1fc907
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-10-21 17:48:17 +0100

    update

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,20 +1,54 @@
-import React from 'react'
-import BottomNav from '../../components/BottomNav'
-import HomeNav from '../../components/HomeNav'
-import { foodNotActive, homeActive, orderNotActive, profileNotActive } from '../../utility/tools'
-import "./Homepage.css"
+import React from "react";
+import BottomNav from "../../components/BottomNav";
+import HomeNav from "../../components/HomeNav";
+import {
+  foodNotActive,
+  homeActive,
+  orderNotActive,
+  plate_icon,
+  profileNotActive,
+} from "../../utility/tools";
+import "./Homepage.css";
 
 function Homepage() {
   return (
     <>
-    <HomeNav/>
-    <div className="homepage__cnt">
+      <HomeNav />
+      <div className="homepage__cnt">
         <p className="greeting">Good Morning 9trocoder,</p>
         <p className="happy">Happy Liasu Clan Day!</p>
-    </div>
-        <BottomNav homeIcon={homeActive} foodIcon={foodNotActive} orderIcon={orderNotActive} profileIcon={profileNotActive} />
+
+        <div className="food__list-carousel">
+          <div className="food__list-items">
+            <div className="food__listfist">
+              {plate_icon} <p>Breakfast</p>
+            </div>{" "}
+          </div>
+          <div className="food__list-items">
+            <p>Lunch</p>
+          </div>
+          <div className="food__list-items">
+            <p>Dinner</p>
+          </div>
+          <div className="food__list-items">
+            <p>Snack</p>
+          </div>
+          <div className="food__list-items">
+            <p>Desert</p>
+          </div>
+          <div className="food__list-items">
+            <p>Drinks</p>
+          </div>
+        </div>
+      </div>
+      <BottomNav
+        homeIcon={homeActive}
+        foodIcon={foodNotActive}
+        orderIcon={orderNotActive}
+        profileIcon={profileNotActive}
+      />
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
